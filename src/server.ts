9d9531e1992a1056114ddc6f0003f0f5d44fe89f
ITSSCOR <160145059+ITSSCOR@users.noreply.github.com>
2024-03-07 14:45:02 +0000

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import weatherRoute from "./routes/weatherRoutes.js";
-
+import cors from "cors"
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors())
 app.use("/api/weather", weatherRoute);
 app.listen(PORT, () => {
   console.log("server is listening on port ${PORT}");
