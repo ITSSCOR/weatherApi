e7c45a0cf7a26ae759557179ccada2e7e14b7e7c
ITSSCOR <160145059+ITSSCOR@users.noreply.github.com>
2024-03-07 14:44:37 +0000

    made changes to connect it to the front end and use cors policy

--- src/routes/weatherRoutes.ts
+++ src/routes/weatherRoutes.ts
@@ -1,9 +1,9 @@
 import express from "express";
- import { getWeatherData } from "../controllers/weatherController.js";
- import { validateCityName } from "../middleware/validators.js";
+import { getWeatherData } from "../controllers/weatherController.js";
+import { validateCityName } from "../middleware/validators.js";
 
- const router = express.Router();
+const router = express.Router();
 
- router.get("/:city", validateCityName, getWeatherData);
+router.get("/:city", validateCityName, getWeatherData);
 
- export default router;
+export default router;
